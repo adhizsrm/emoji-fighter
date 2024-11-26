811788fab07ba9402a391c60c34dbd3c2d5406f9
adhizsrm <adhizsrm@gmail.com>
2024-11-26 12:41:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let randomVar1 = Math.floor(Math.random()*fighters.length)
+    let randomVar2 = Math.floor(Math.random()*fighters.length)
+    stageEl.textContent = fighters[randomVar1] + "Vs" + fighters[randomVar2]
+    
+})
